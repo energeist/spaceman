c3c3c1a3b147536319833c147fc963655dc36e7b
Mark Rattle <mark.rattle@gmail.com>
2022-10-02 14:34:29 -0400

    added scoreboard

--- spaceman.py
+++ spaceman.py
@@ -99,7 +99,7 @@ def compress_string(letters_guessed):
         compressed_string += letter
     return compressed_string
 
-def spaceman(secret_word):
+def spaceman(secret_word, total_score):
 
     # A function that controls the game of spaceman. Will start spaceman in the command line.
     # Args:
@@ -113,12 +113,16 @@ def spaceman(secret_word):
     guess = ''
     letters_guessed = []
     remaining_letters = []
+
+    #total score index 0 = wins, index 1 = losses
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     for letter in alphabet:
         remaining_letters.append(letter)
 
     for character in secret_word:
         letters_guessed.append('_')
+
+    print(f"The secret word is {len(secret_word)} characters long.")
 
     while incorrect_guesses < max_incorrect_guesses:
         print(f"Letters guessed in the secret word: {compress_string(letters_guessed)}")
@@ -132,13 +136,17 @@ def spaceman(secret_word):
             print(f"Your guess [ {guess} ] was \33[32mcorrect\33[0m!")
             if is_word_guessed(secret_word, letters_guessed) == True:
                 print(f"\33[32mYou've guessed the secret word!\33[0m  It was '{secret_word}'!")
+                total_score[0] += 1
                 break
         else:
             incorrect_guesses += 1
             if incorrect_guesses == max_incorrect_guesses:
                 print(f"Sorry, you've made \33[31m{max_incorrect_guesses} incorrect guesses\33[0m! and the game is over.  The secret word was '{secret_word}'")
+                print()
+                total_score[1] += 1
             else:
                 print(f"Your guess [ {guess} ] was \33[31mnot correct\33[0m!  You have {max_incorrect_guesses - incorrect_guesses} incorrect remaining.")
+    return total_score
 
 
             
@@ -185,10 +193,12 @@ Good luck!
 # These function calls that will start and loop the game until the player is done
 
 play_again = True
+total_score = [0, 0]
+
 while play_again == True:
     letters_guessed = ''
     secret_word = load_word()
-    spaceman(secret_word)
+    spaceman(secret_word, total_score)
     play_again_prompt = input("Would you like to play again? Enter y / n > ")
     while play_again_prompt.lower() != 'y' and play_again_prompt.lower() != 'n':
         play_again_prompt = input("Invalid input. Would you likke to play again? Enter y / n > ")
@@ -196,7 +206,9 @@ while play_again == True:
         play_again = True
     else:
         play_again = False
-print("Thanks for playing!")
+    sleep(1.5)
+    system('clear')
+print(f"Thanks for playing! Your final score was \33[32m{total_score[0]} wins\33[0m and \33[31m{total_score[1]} losses\33[0m.")
 
 
 
